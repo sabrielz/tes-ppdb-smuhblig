d247f6adb1254138b719242d8d7fffe4dc884e3f
Sabriel <sadewobrilianrafael@gmail.com>
2023-04-02 19:58:36 +0700

    merge modified file in question controller

--- resources/views/pages/dashboard/question/index.blade.php
+++ resources/views/pages/dashboard/question/index.blade.php
@@ -1,2 +1,37 @@
 @extends('layouts.dashboard')
 
+@section('content')
+	<div class="row">
+
+		<div class="col-12 card card-default">
+			<div class="card-body">
+				<table class="table w-100">
+					<thead>
+						<th>No.</th>
+						<th>Pertanyaan</th>
+						<th>Jawaban</th>
+						<th>Tindakan</th>
+					</thead>
+					<tbody>
+						@foreach ($questions as $quest)
+							<tr>
+								<td>{{ $loop->iteration }}</td>
+								<td>{{ $quest->question }}</td>
+								<td>{{ 'No field yet' }}</td>
+								<td>
+									<div class="btn-group btn-group-sm">
+										<a href="javascript:void()" class="btn btn-default">
+											<i class="fa fa-pen"></i>
+										</a>
+									</div>
+								</td>
+							</tr>
+						@endforeach
+					</tbody>
+					<tfoot></tfoot>
+				</table>
+			</div>
+		</div>
+
+	</div>
+@endsection
