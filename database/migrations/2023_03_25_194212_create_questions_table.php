f376f9339514a7f755de58329ef5fa069a82b6c4
NafisFadhil <nafisfadhil28@gmail.com>
2023-04-02 21:12:42 +0700

    update migration question

--- database/migrations/2023_03_25_194212_create_questions_table.php
+++ database/migrations/2023_03_25_194212_create_questions_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
 						$table->foreignId('type_id');
 						$table->text('question');
+						$table->text('answer')->nullable();
             $table->timestamps();
 						$table->dateTime('deleted_at')->nullable();
         });
