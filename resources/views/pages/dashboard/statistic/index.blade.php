f2c9e019ed54639366464afb6b943e2dd5ba2651
NafisFadhil <nafisfadhil28@gmail.com>
2023-04-07 09:36:23 +0700

    update ajax

--- resources/views/pages/dashboard/statistic/index.blade.php
+++ resources/views/pages/dashboard/statistic/index.blade.php
@@ -79,199 +79,6 @@
 									</div>
 								</td>
 						@endforeach
-
-						@push('html_scripts')
-											{{-- <script>
-												$('#detail-siswa-{{ $loop->iteration }}').on('click', function() {
-													let id = $(this).data('id')
-													$.ajax({
-														type: "get",
-														url: "{{ route('api.detail.index') }}",
-														data: {
-															id: id
-														},
-														success: function (response) {
-															$('#detail-body').html(`
-																<table class="table table-sm table-borderless w-auto">
-																	<tbody>
-																			<tr>
-																				<th>Nama Lengkap</th>
-																				<td>:</td>
-																				<td>${response.data.nama_lengkap}</td>
-																			</tr>
-																			<tr>
-																				<th>Alamat</th>
-																				<td>:</td>
-																				<td>${response.data.alamat_desa}, ${response.data.alamat_kec}, ${response.data.alamat_kota_kab}, RT ${response.data.alamat_rt}, RW ${response.data.alamat_rw}</td>
-																			</tr>
-																			<tr>
-																				<th>Tempat / Tanggal Lahir</th>
-																				<td>:</td>
-																				<td>${response.data.tempat_lahir} / ${response.data.tanggal_lahir}</td>
-																			</tr>
-																			<tr>
-																				<th>Jenis Kelamin</th>
-																				<td>:</td>
-																				<td>${response.data.jenis_kelamin.label}</td>
-																			</tr>
-																			<tr>
-																				<th>Asal Sekolah</th>
-																				<td>:</td>
-																				<td>${response.data.asal_sekolah}</td>
-																			</tr>
-																			<tr>
-																				<th>Nomor WA</th>
-																				<td>:</td>
-																				<td>${response.data.no_wa_siswa}</td>
-																			</tr>
-																			<tr>
-																				<th>WA Orang Tua</th>
-																				<td>:</td>
-																				<td>${response.data.no_wa_ortu ?? '-'}</td>
-																			</tr>
-																			<tr>
-																				<th>Nama Ayah</th>
-																				<td>:</td>
-																				<td>${response.data.nama_ayah}</td>
-																			</tr>
-																			<tr>
-																				<th>Nama Ibu</th>
-																				<td>:</td>
-																				<td>${response.data.nama_ibu}</td>
-																			</tr>
-																			<tr>
-																				<th>Tahun Lahir Ayah</th>
-																				<td>:</td>
-																				<td>${response.data.tahun_lahir_ayah ?? '-'}</td>
-																			</tr>
-																			<tr>
-																				<th>Tahun Lahir Ibu</th>
-																				<td>:</td>
-																				<td>${response.data.tahun_lahir_ibu ?? '-'}</td>
-																			</tr>
-																			<tr>
-																				<th>NIK</th>
-																				<td>:</td>
-																				<td>${response.data.nik ?? '-'}</td>
-																			</tr>
-																			<tr>
-																				<th>NISN</th>
-																				<td>:</td>
-																				<td>${response.data.nisn ?? '-'}</td>
-																			</tr>
-																			<tr>
-																				<th>No. Ijazah</th>
-																				<td>:</td>
-																				<td>${response.data.no_ijazah ?? '-'}</td>
-																			</tr>
-																			<tr>
-																				<th>No. Ujian Nasional</th>
-																				<td>:</td>
-																				<td>${response.data.no_ujian_nasional ?? '-'}</td>
-																			</tr>
-																	</tbody>
-																</table>
-															`);
-														}
-													});
-												})
-											</script> --}}
-
-											<script>
-												const fetchData = (id) => {
-													$.ajax({
-														type: "get",
-														url: "{{ route('api.detail.index') }}",
-														data: {
-															id: id
-														},
-														success: function (response) {
-															$('#detail-body').html(`
-																<table class="table table-sm table-borderless w-auto">
-																	<tbody>
-																			<tr>
-																				<th>Nama Lengkap</th>
-																				<td>:</td>
-																				<td>${response.data.nama_lengkap}</td>
-																			</tr>
-																			<tr>
-																				<th>Alamat</th>
-																				<td>:</td>
-																				<td>${response.data.alamat_desa}, ${response.data.alamat_kec}, ${response.data.alamat_kota_kab}, RT ${response.data.alamat_rt}, RW ${response.data.alamat_rw}</td>
-																			</tr>
-																			<tr>
-																				<th>Tempat / Tanggal Lahir</th>
-																				<td>:</td>
-																				<td>${response.data.tempat_lahir} / ${response.data.tanggal_lahir}</td>
-																			</tr>
-																			<tr>
-																				<th>Jenis Kelamin</th>
-																				<td>:</td>
-																				<td>${response.data.jenis_kelamin.label}</td>
-																			</tr>
-																			<tr>
-																				<th>Asal Sekolah</th>
-																				<td>:</td>
-																				<td>${response.data.asal_sekolah}</td>
-																			</tr>
-																			<tr>
-																				<th>Nomor WA</th>
-																				<td>:</td>
-																				<td>${response.data.no_wa_siswa}</td>
-																			</tr>
-																			<tr>
-																				<th>WA Orang Tua</th>
-																				<td>:</td>
-																				<td>${response.data.no_wa_ortu ?? '-'}</td>
-																			</tr>
-																			<tr>
-																				<th>Nama Ayah</th>
-																				<td>:</td>
-																				<td>${response.data.nama_ayah}</td>
-																			</tr>
-																			<tr>
-																				<th>Nama Ibu</th>
-																				<td>:</td>
-																				<td>${response.data.nama_ibu}</td>
-																			</tr>
-																			<tr>
-																				<th>Tahun Lahir Ayah</th>
-																				<td>:</td>
-																				<td>${response.data.tahun_lahir_ayah ?? '-'}</td>
-																			</tr>
-																			<tr>
-																				<th>Tahun Lahir Ibu</th>
-																				<td>:</td>
-																				<td>${response.data.tahun_lahir_ibu ?? '-'}</td>
-																			</tr>
-																			<tr>
-																				<th>NIK</th>
-																				<td>:</td>
-																				<td>${response.data.nik ?? '-'}</td>
-																			</tr>
-																			<tr>
-																				<th>NISN</th>
-																				<td>:</td>
-																				<td>${response.data.nisn ?? '-'}</td>
-																			</tr>
-																			<tr>
-																				<th>No. Ijazah</th>
-																				<td>:</td>
-																				<td>${response.data.no_ijazah ?? '-'}</td>
-																			</tr>
-																			<tr>
-																				<th>No. Ujian Nasional</th>
-																				<td>:</td>
-																				<td>${response.data.no_ujian_nasional ?? '-'}</td>
-																			</tr>
-																	</tbody>
-																</table>
-															`);
-														}
-													});
-												}
-											</script>
-										@endpush
 					</tbody>
 				</table>
 			</div>
@@ -288,4 +95,197 @@
 
 @push('html_modals')
 	<x-modal.detail-siswa />
+@endpush
+
+@push('html_scripts')
+	{{-- <script>
+		$('#detail-siswa-{{ $loop->iteration }}').on('click', function() {
+			let id = $(this).data('id')
+			$.ajax({
+				type: "get",
+				url: "{{ route('api.detail.index') }}",
+				data: {
+					id: id
+				},
+				success: function (response) {
+					$('#detail-body').html(`
+						<table class="table table-sm table-borderless w-auto">
+							<tbody>
+									<tr>
+										<th>Nama Lengkap</th>
+										<td>:</td>
+										<td>${response.data.nama_lengkap}</td>
+									</tr>
+									<tr>
+										<th>Alamat</th>
+										<td>:</td>
+										<td>${response.data.alamat_desa}, ${response.data.alamat_kec}, ${response.data.alamat_kota_kab}, RT ${response.data.alamat_rt}, RW ${response.data.alamat_rw}</td>
+									</tr>
+									<tr>
+										<th>Tempat / Tanggal Lahir</th>
+										<td>:</td>
+										<td>${response.data.tempat_lahir} / ${response.data.tanggal_lahir}</td>
+									</tr>
+									<tr>
+										<th>Jenis Kelamin</th>
+										<td>:</td>
+										<td>${response.data.jenis_kelamin.label}</td>
+									</tr>
+									<tr>
+										<th>Asal Sekolah</th>
+										<td>:</td>
+										<td>${response.data.asal_sekolah}</td>
+									</tr>
+									<tr>
+										<th>Nomor WA</th>
+										<td>:</td>
+										<td>${response.data.no_wa_siswa}</td>
+									</tr>
+									<tr>
+										<th>WA Orang Tua</th>
+										<td>:</td>
+										<td>${response.data.no_wa_ortu ?? '-'}</td>
+									</tr>
+									<tr>
+										<th>Nama Ayah</th>
+										<td>:</td>
+										<td>${response.data.nama_ayah}</td>
+									</tr>
+									<tr>
+										<th>Nama Ibu</th>
+										<td>:</td>
+										<td>${response.data.nama_ibu}</td>
+									</tr>
+									<tr>
+										<th>Tahun Lahir Ayah</th>
+										<td>:</td>
+										<td>${response.data.tahun_lahir_ayah ?? '-'}</td>
+									</tr>
+									<tr>
+										<th>Tahun Lahir Ibu</th>
+										<td>:</td>
+										<td>${response.data.tahun_lahir_ibu ?? '-'}</td>
+									</tr>
+									<tr>
+										<th>NIK</th>
+										<td>:</td>
+										<td>${response.data.nik ?? '-'}</td>
+									</tr>
+									<tr>
+										<th>NISN</th>
+										<td>:</td>
+										<td>${response.data.nisn ?? '-'}</td>
+									</tr>
+									<tr>
+										<th>No. Ijazah</th>
+										<td>:</td>
+										<td>${response.data.no_ijazah ?? '-'}</td>
+									</tr>
+									<tr>
+										<th>No. Ujian Nasional</th>
+										<td>:</td>
+										<td>${response.data.no_ujian_nasional ?? '-'}</td>
+									</tr>
+							</tbody>
+						</table>
+					`);
+				}
+			});
+		})
+	</script> --}}
+
+	<script>
+		const fetchData = (id) => {
+			$.ajax({
+				type: "get",
+				url: "{{ route('api.detail.index') }}",
+				data: {
+					id: id
+				},
+				success: function (response) {
+					$('#detail-body').html(`
+						<table class="table table-sm table-borderless w-auto">
+							<tbody>
+									<tr>
+										<th>Nama Lengkap</th>
+										<td>:</td>
+										<td>${response.data.nama_lengkap}</td>
+									</tr>
+									<tr>
+										<th>Alamat</th>
+										<td>:</td>
+										<td>${response.data.alamat_desa}, ${response.data.alamat_kec}, ${response.data.alamat_kota_kab}, RT ${response.data.alamat_rt}, RW ${response.data.alamat_rw}</td>
+									</tr>
+									<tr>
+										<th>Tempat / Tanggal Lahir</th>
+										<td>:</td>
+										<td>${response.data.tempat_lahir} / ${response.data.tanggal_lahir}</td>
+									</tr>
+									<tr>
+										<th>Jenis Kelamin</th>
+										<td>:</td>
+										<td>${response.data.jenis_kelamin.label}</td>
+									</tr>
+									<tr>
+										<th>Asal Sekolah</th>
+										<td>:</td>
+										<td>${response.data.asal_sekolah}</td>
+									</tr>
+									<tr>
+										<th>Nomor WA</th>
+										<td>:</td>
+										<td>${response.data.no_wa_siswa}</td>
+									</tr>
+									<tr>
+										<th>WA Orang Tua</th>
+										<td>:</td>
+										<td>${response.data.no_wa_ortu ?? '-'}</td>
+									</tr>
+									<tr>
+										<th>Nama Ayah</th>
+										<td>:</td>
+										<td>${response.data.nama_ayah}</td>
+									</tr>
+									<tr>
+										<th>Nama Ibu</th>
+										<td>:</td>
+										<td>${response.data.nama_ibu}</td>
+									</tr>
+									<tr>
+										<th>Tahun Lahir Ayah</th>
+										<td>:</td>
+										<td>${response.data.tahun_lahir_ayah ?? '-'}</td>
+									</tr>
+									<tr>
+										<th>Tahun Lahir Ibu</th>
+										<td>:</td>
+										<td>${response.data.tahun_lahir_ibu ?? '-'}</td>
+									</tr>
+									<tr>
+										<th>NIK</th>
+										<td>:</td>
+										<td>${response.data.nik ?? '-'}</td>
+									</tr>
+									<tr>
+										<th>NISN</th>
+										<td>:</td>
+										<td>${response.data.nisn ?? '-'}</td>
+									</tr>
+									<tr>
+										<th>No. Ijazah</th>
+										<td>:</td>
+										<td>${response.data.no_ijazah ?? '-'}</td>
+									</tr>
+									<tr>
+										<th>No. Ujian Nasional</th>
+										<td>:</td>
+										<td>${response.data.no_ujian_nasional ?? '-'}</td>
+									</tr>
+							</tbody>
+						</table>
+					`);
+				}
+			});
+		}
+	</script>
 @endpush
